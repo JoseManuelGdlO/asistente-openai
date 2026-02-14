c756ec9c275e012beb7b6b840c795c5dac72a470
Jose Manuel <JoseManuel@Icloud.com>
2026-02-14 09:53:18 -0600

    Add application version logging in index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const multer = require('multer');
 require('dotenv').config();
 
+const pkg = require('../package.json');
+const APP_VERSION = pkg.version;
+
 // Importar todas las clases
 const UltraMsgManager = require('./managers/ultramsgManager');
 const Scheduler = require('./services/scheduler');
@@ -1019,6 +1022,7 @@ app.post('/test', (req, res) => {
 
 app.listen(port, async () => {
     console.log('=== SERVER STARTED ===');
+    console.log(`Versión: ${APP_VERSION}`);
     console.log(`Server is running on port ${port}`);
     console.log(`Webhook URL: http://localhost:${port}/webhook`);
     console.log(`Test URL: http://localhost:${port}/test`);
